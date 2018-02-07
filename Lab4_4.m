207f981f1f8f20eb451f01fcccbab15082f1d239
EttNollEtt <33880192+EttNollEtt@users.noreply.github.com>
2018-02-07 12:33:56 +0100

    Add files via upload

---
+++ Lab4_4.m
@@ -0,0 +1,39 @@
+%
+%
+%
+
+% a1)
+format long;
+fk = @(x) sqrt(x + 4);
+h = 0.5;
+hH = 1;
+hHH = 2;
+
+% a2)
+nollFem = h*(fk(-1)/2 + fk(-1+h) + fk(-1+2*h) + fk(-1+3*h) + fk(1)/2);
+ettNoll = hH*(fk(-1)/2 + fk(-1+hH) + fk(1)/2);
+tvaNoll = hHH*(fk(-1)/2 + fk(1)/2);
+
+% Richardsonextrapolation
+% a3)
+Fs = nollFem + (nollFem-ettNoll)/((2^9)-1);
+
+% Test av feltermer
+val_ett = nollFem;
+val_two = ettNoll;
+val_three = tvaNoll;
+val_korr = (10*sqrt(5)/3)-2*sqrt(3);
+
+err_ett = abs(val_korr - val_ett)
+err_two = abs(val_korr - val_two)
+err_three = abs(val_korr - val_three)
+err_rich = abs(val_korr - Fs)
+
+y_val = [err_ett err_two err_three err_rich];
+x_val = [h hH hHH hHH];
+
+loglog(x_val, y_val);
+hold on
+plot(h, err_ett, '*', hH, err_two, '+', hHH, err_three, 'x', h, err_rich, 'd');
+
+% b)
