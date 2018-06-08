64adba90d0161fad91f6f31c9d5ab4238235c9f2
0KevinK <33936552+0KevinK@users.noreply.github.com>
2018-06-08 14:35:05 +0200

    safh p9wnda

--- richard.m
+++ richard.m
@@ -13,6 +13,7 @@ tvaNoll = hHH*(fk(-1)/2 + fk(1)/2);
 % Richardsonextrapolation
 % a3)
 Fs = nollFem + (nollFem-ettNoll)/((2^2)-1);
+Fs2 = Fs + (Fs-nollFem)/3;
 
 % Test av feltermer
 val_one = nollFem;
@@ -24,13 +25,13 @@ err_one = abs(val_korr - val_one);
 err_two = abs(val_korr - val_two);
 err_three = abs(val_korr - val_three);
 err_rich = abs(val_korr - Fs);
+% err_rich2 = abs(val_korr - Fs2);
 
-y_val = [err_rich err_one];
+y_val = [err_rich err_rich2];
 x_val = [h hHH];
-% loglog(x_val, y_val);
-hold on
+loglog(x_val, y_val);
 fk = @ (x) (x^4);
-yyval = [fk(err_rich) fk(err_one)];
-plot(x_val, [err_rich err_three])
-figure(2)
-plot(x_val, [0:(2/3):2].^2);
+xsq = [fk(0) fk(2)];
+% loglog(x_val, [err_rich err_three])
+hold on
+loglog(x_val, xsq);
